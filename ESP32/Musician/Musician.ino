4889314349bca0fe34c3bc876db6e3f986fa4830
Joe Andolina <jandolina@salesforce.com>
2018-05-13 18:18:55 -0700

    Adding ssid manager

--- ESP32/Musician/Musician.ino
+++ ESP32/Musician/Musician.ino
@@ -19,6 +19,7 @@ QueueArray <int> notes;
 Ticker gateCloseTask, gateOpenTask, reportTask;
 
 bool dropTest = false;
+bool shouldConnect = false;
 
 // Interrupts and debouncing
 unsigned long tempo = 300;
@@ -43,14 +44,12 @@ void setup() {
   Serial.begin(115200);
   delay(100);
   Serial.print("");
-
-  startPixel();
-  delay(10000);
-  setIndicating();
-  delay(10000);
-  setAlerting();
-  /*
-  //OTAManager::getInstance().clearPreferences();
+  
+  uint64_t chipHex=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
+  String chipId = chipIdToString(chipHex).substring(10);
+  Serial.println("ESP32 Chip ID = " + chipId);//print High 2 bytes
+  
+  OTAManager::getInstance().clearPreferences();
   OTAManager::getInstance().start();
   
   pinMode(NOTE_PIN, OUTPUT);
@@ -63,19 +62,33 @@ void setup() {
   touchAttachInterrupt(T2, gotTouch1, (10+touchRead(T2))/2);
   touchAttachInterrupt(T3, gotTouch2, (10+touchRead(T3))/2);
 
-  delay(15000);
   ESP_LOGI(LOG_TAG,"Connecting to Symphony");
+  //connection.init();
   connection.onMessage(handleMessage);
   
-  if (connection.connect()) {
-    ESP_LOGI(LOG_TAG,"Successfully established Symphony Connection");
-    
-  } else {
-    ESP_LOGI(LOG_TAG,"Failed to establish Symphony Connection");
-  }
-  */
+  //if (connection.connect()) {
+  //  ESP_LOGI(LOG_TAG,"Successfully established Symphony Connection");
+  //} else {
+  //  ESP_LOGI(LOG_TAG,"Failed to establish Symphony Connection");
+  //}
 }
 
+String chipIdToString(uint64_t input) {
+  String result = "";
+  uint8_t base = 10;
+
+  do {
+    char c = input % base;
+    input /= base;
+
+    if (c < 10)
+      c +='0';
+    else
+      c += 'A' - 10;
+    result = c + result;
+  } while (input);
+  return result;
+}
 /**
  * This callback will be in invoked for messages from server.
  */
@@ -198,7 +211,10 @@ void gotTouch2(){
 /* ------------------------------------------------------------------- */
 void handleButton() {
   if (lastButtonTime > millis()) return;
-  
+  ESP_LOGI(LOG_TAG,"Connected at: %d", connection.isConnected());
+  if(!connection.isConnected()){
+    shouldConnect = true;
+  }
   lastButtonTime = bounceDelay + millis();
   gateOpen();
 }
@@ -262,5 +278,10 @@ void sendReport(){
 /* ----- Main Loop  ---------------------------------------------- */
 
 void loop() {
-  vTaskDelay(portMAX_DELAY);
+  connection.workConnection();
+  if(shouldConnect){
+    shouldConnect = false;
+    connection.init();
+  }
+  //vTaskDelay(portMAX_DELAY);
 }
--- ESP32/ssid_publisher/ssid_publisher.ino
+++ ESP32/ssid_publisher/ssid_publisher.ino
@@ -2,30 +2,45 @@
     Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
     Ported to Arduino ESP32 by Evandro Copercini
 */
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
+#include <SPI.h>
+#include <Wire.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+// Display SPI Pins
+#define OLED_MOSI  13
+#define OLED_CLK   14
+#define OLED_DC    5
+#define OLED_CS    4
+#define OLED_RESET 21
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
 
 #define SERVICE_UUID        "78b940fc-131d-4cc5-961c-891153c0cce7"
 #define SSID_UUID           "cd10d9cc-f8d5-468d-89a1-aac3149ab4f9"
 #define PASS_UUID           "d8b8a3e7-2361-4b67-bf51-40b1efd340c0"
 
-//#define SERVICE_SSID        "Rachel's Network"
-//#define SERVICE_PASS        "photoshop!"
+#define SERVICE_SSID        "Rachel's Network"
+#define SERVICE_PASS        "photoshop!"
 
-#define SERVICE_SSID        "Verizon-MiFi6620L-D537"
-#define SERVICE_PASS        "a71745e9"
+//#define SERVICE_SSID        "Verizon-MiFi6620L-D537"
+//#define SERVICE_PASS        "a71745e9"
+
+#define CHAR_MAX 9
+int charCount = 0;
 
 void setup() {
-  Serial.begin(115200);
-  delay(100);
-  Serial.println("Starting BLE work!");
-  BLEDevice::init("SymphonyConductor");
+  display.begin(SSD1306_SWITCHCAPVCC);
+  display.clearDisplay();
+  display.display();
+  display.setTextColor(WHITE);
   
-  Serial.print("BLE ADDRESS");
-  Serial.println(BLEDevice::getAddress().toString().c_str());
+  BLEDevice::init("SymphonyConductor");
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
   BLECharacteristic *ssidCharacteristic = pService->createCharacteristic(
@@ -42,10 +57,31 @@ void setup() {
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->start();
-  Serial.println("Characteristic defined! Now you can read it!");
+}
+
+void updateDisplay(){
+  String ssid = String(SERVICE_SSID);
+  if(ssid.length() > 20){
+    ssid = ssid.substring(0,17) + "...";  
+  }
+  
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.setTextSize(1);
+  display.println("SSID Publisher\n\n" + ssid +"\n");
+  display.println(BLEDevice::getAddress().toString().c_str());
+  display.println("");
+  display.setTextSize(2);
+  for(int i=0; i<charCount; i++){
+    display.print(".");  
+  }
+  display.display();
 }
 
 void loop() {
-  Serial.println("Characteristic Server Alive.");
-  delay(10000);
+  if(++charCount == CHAR_MAX){
+    charCount = 0;  
+  }
+  updateDisplay();
+  delay(2000);
 }
